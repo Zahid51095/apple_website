b0c9343a98c7073bbf6aba8745ec069e58794f10
Zahid Hasan <zahidhasan140338@gmail.com>
2024-10-26 22:47:44 +0600

    some extra files has been imported for the camera purpose

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import ReactThree from 'eslint-plugin-react-three'
 
 export default [
   { ignores: ['dist'] },
@@ -22,6 +23,7 @@ export default [
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'react-three': ReactThree,
     },
     rules: {
       ...js.configs.recommended.rules,
--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,8 +1,35 @@
+import { OrbitControls, PerspectiveCamera, View } from "@react-three/drei"
+import Lights from "./Lights"
+import { Suspense } from "react"
+import IPhone from './IPhone';
 
 
-const ModelView = () => {
+const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, size, item }) => {
   return (
-    <div>ModelView</div>
+    <View
+    index={index}
+    id={gsapType}
+    className={`w-full h-full absolute ${index === 2 ? 'right-[-100%]' : ''}`}
+    >
+
+     {/* Ambient Light */}
+      <ambientLight intensity={0.3} />
+
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+
+      <Lights></Lights>
+
+      <OrbitControls></OrbitControls>
+
+      <group  ref={groupRef} name={`${index === 1} ? 'small' : 'large`} position={[0, 0 ,0]}>
+
+      <Suspense fallback={<div>Loading</div>}>
+        <IPhone></IPhone>
+
+      </Suspense>
+      </group>
+     
+    </View>
   )
 }
 
